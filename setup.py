4d9e03b783317570972be3d404d3f36d19d22272
Sam <sangyoonkim0708@gmail.com>
2021-05-02 07:03:24 -0700

    fixed typo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="cmc_csci046_sangyoon_containers",
-    version="1.0.0",
+    version="1.1.0",
     description="Tree project for CSCI046 data structures course at CMC.",
     long_description=README,
     long_description_content_type="text/markdown",
